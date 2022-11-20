eaffc532ac142582234d65cd79517702db59e5e3
Sailin ZHONG <sailinzhong@gmail.com>
2022-11-20 17:58:01 -0500

    Initial Commit

---
+++ AirSpec_iOS Watch App/AirSpec_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirSpec_iOSApp.swift
+//  AirSpec_iOS Watch App
+//
+//  Created by ZHONG Sailin on 20.11.22.
+//
+
+import SwiftUI
+
+@main
+struct AirSpec_iOS_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AirSpec_iOS/AirSpec_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirSpec_iOSApp.swift
+//  AirSpec_iOS
+//
+//  Created by ZHONG Sailin on 20.11.22.
+//
+
+import SwiftUI
+
+@main
+struct AirSpec_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
